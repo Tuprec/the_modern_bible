7e289eadc9e508216dc105da0003969fe9fc2a1c
tuprec <thibe.provost@student.odisee.com>
2023-06-01 18:14:18 +0200

    db

--- server/db.js
+++ server/db.js
@@ -4,8 +4,8 @@ import mysql from 'mysql';
 
 // Create a connection pool
 export const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'express',
-    password: 'Azerty123',
+    host: 'localhost:3306',
+    user: 'express1',
+    password: 'Azerty123321!',
     database: 'apireviews',
 });
